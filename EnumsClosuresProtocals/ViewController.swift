24f871b7a2e483f01aaf77ba2ccda3bfe3de5b12
teja <tejagoud.ios@gmail.com>
2021-09-29 13:23:12 +1000

    adding enumeration code to git

--- EnumsClosuresProtocals/ViewController.swift
+++ EnumsClosuresProtocals/ViewController.swift
@@ -11,9 +11,69 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     // let result = trainBerthNames(berths: .upper)
+        let result = trainBerthNames1(berths1: .upper)
+        print(result)
+        
+        let dataCode = dataModel(codes: .barCode(10, 10, "teja you are intillent"))
+        print(dataCode)
+    }
+//MARK:-ENUMARATIONS
+    enum TrainBerth {
+    case upper, lower, middle, any
+    }
+    func trainBerthNames(berths:TrainBerth) -> String {
+        switch berths {
+        case .upper:
+            return "oops upper berth"
+        case .lower:
+            return "oops lower berth"
+
+        case .middle:
+            return "oops middle berth"
+
+        case .any:
+            return "oops any berth"
+        }
+    }
+    enum trainBerth1: String {
+        case upper = "you booked upper seat"
+        case lower = "you booked lower seat"
+        case middle = "you booked middle seat"
+        case any = "you booked any seat"
+
+    }
+    func trainBerthNames1(berths1: trainBerth1) -> String {
+        switch berths1 {
+        case .upper:
+            return "\(trainBerth1.upper.rawValue)"
+        case .lower:
+            return "\(trainBerth1.lower.rawValue)"
+
+        case .middle:
+            return "\(trainBerth1.middle.rawValue)"
+
+        case .any:
+            return "\(trainBerth1.any.rawValue)"
+
+        }
+    }
+    
+    enum QRCode {
+    case barCode( Int, Int , String )
+        case navigationBar(String)
+    }
+    func dataModel(codes: QRCode) -> Any {
+        switch codes {
+        case .barCode(let a, let b, let desc):
+            return "\(a * b) \(desc)"
+        case .navigationBar(let name):
+            return name
+        }
     }
 
-
+    
+        
 }
+
 
